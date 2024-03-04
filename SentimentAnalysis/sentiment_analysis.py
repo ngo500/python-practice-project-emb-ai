e2c8d5587ccc8ace97d0bf16ae66660d6137f211
Nikki Gowan <nikkigowan2@gmail.com>
2024-03-04 16:48:35 -0500

    Update sentiment_analysis.py, empty input handling

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -2,14 +2,16 @@
 import json
 import requests
 
-def sentiment_analyzer(text_to_analyse):
+def sentiment_analyzer(text_to_analyze):
     """
     method that sends argument text to Watson NLP BERT sentiment analysis function to be analyzed
     returns:
     formatted label and score from Watson NLP
     """
+    if not text_to_analyze:
+        return { 'label' : "empty", 'score' : "empty" }
     url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
-    myobj = { "raw_document" : { "text" : text_to_analyse } }
+    myobj = { "raw_document" : { "text" : text_to_analyze } }
     header = { "grpc-metadata-mm-model-id" : "sentiment_aggregated-bert-workflow_lang_multi_stock" }
     #send info over POST request and store response
     response = requests.post(url, json = myobj, headers = header, timeout = 10)
